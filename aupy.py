574e76651f315c650e92f863a6413445bc562504
Fred Gruber <fred@gnshealthcare.com>
2020-02-02 21:19:00 -0500

    added pythong script with autoencoder

---
+++ aupy.py
@@ -0,0 +1,134 @@
+from numpy.random import seed
+##seed(123)
+import sklearn
+from sklearn import datasets
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn import decomposition
+import scipy
+import tensorflow as tf
+from keras.models import Model, load_model
+from keras.layers import Input, Dense, Layer, InputSpec, Dropout, BatchNormalization
+from keras.callbacks import ModelCheckpoint, TensorBoard
+from keras import regularizers, activations, initializers, constraints, Sequential
+from keras import backend as K
+from keras.constraints import UnitNorm, Constraint
+import pandas as pd
+class DenseTied(Layer):
+    def __init__(self, units,
+                 activation=None,
+                 use_bias=True,
+                 kernel_initializer='glorot_uniform',
+                 bias_initializer='zeros',
+                 kernel_regularizer=None,
+                 bias_regularizer=None,
+                 activity_regularizer=None,
+                 kernel_constraint=None,
+                 bias_constraint=None,
+                 tied_to=None,
+                 **kwargs):
+        self.tied_to = tied_to
+        if 'input_shape' not in kwargs and 'input_dim' in kwargs:
+            kwargs['input_shape'] = (kwargs.pop('input_dim'),)
+        super().__init__(**kwargs)
+        self.units = units
+        self.activation = activations.get(activation)
+        self.use_bias = use_bias
+        self.kernel_initializer = initializers.get(kernel_initializer)
+        self.bias_initializer = initializers.get(bias_initializer)
+        self.kernel_regularizer = regularizers.get(kernel_regularizer)
+        self.bias_regularizer = regularizers.get(bias_regularizer)
+        self.activity_regularizer = regularizers.get(activity_regularizer)
+        self.kernel_constraint = constraints.get(kernel_constraint)
+        self.bias_constraint = constraints.get(bias_constraint)
+        self.input_spec = InputSpec(min_ndim=2)
+        self.supports_masking = True
+                
+    def build(self, input_shape):
+        assert len(input_shape) >= 2
+        input_dim = input_shape[-1]
+
+        if self.tied_to is not None:
+            self.kernel = K.transpose(self.tied_to.kernel)
+            self._non_trainable_weights.append(self.kernel)
+        else:
+            self.kernel = self.add_weight(shape=(input_dim, self.units),
+                                          initializer=self.kernel_initializer,
+                                          name='kernel',
+                                          regularizer=self.kernel_regularizer,
+                                          constraint=self.kernel_constraint)
+        if self.use_bias:
+            self.bias = self.add_weight(shape=(self.units,),
+                                        initializer=self.bias_initializer,
+                                        name='bias',
+                                        regularizer=self.bias_regularizer,
+                                        constraint=self.bias_constraint)
+        else:
+            self.bias = None
+        self.input_spec = InputSpec(min_ndim=2, axes={-1: input_dim})
+        self.built = True
+
+    def compute_output_shape(self, input_shape):
+        assert input_shape and len(input_shape) >= 2
+        output_shape = list(input_shape)
+        output_shape[-1] = self.units
+        return tuple(output_shape)
+
+    def call(self, inputs):
+        output = K.dot(inputs, self.kernel)
+        if self.use_bias:
+            output = K.bias_add(output, self.bias, data_format='channels_last')
+        if self.activation is not None:
+            output = self.activation(output)
+        return output
+
+
+class WeightsOrthogonalityConstraint (Constraint):
+    def __init__(self, encoding_dim, weightage = 1.0, axis = 0):
+        self.encoding_dim = encoding_dim
+        self.weightage = weightage
+        self.axis = axis
+        
+    def weights_orthogonality(self, w):
+        if(self.axis==1):
+            w = K.transpose(w)
+        if(self.encoding_dim > 1):
+            m = K.dot(K.transpose(w), w) - K.eye(self.encoding_dim)
+            return self.weightage * K.sqrt(K.sum(K.square(m)))
+        else:
+            m = K.sum(w ** 2) - 1.
+            return m
+
+    def __call__(self, w):
+        return self.weights_orthogonality(w)
+
+class UncorrelatedFeaturesConstraint (Constraint):
+    
+    def __init__(self, encoding_dim, weightage = 1.0):
+        self.encoding_dim = encoding_dim
+        self.weightage = weightage
+    
+    def get_covariance(self, x):
+        x_centered_list = []
+
+        for i in range(self.encoding_dim):
+            x_centered_list.append(x[:, i] - K.mean(x[:, i]))
+        
+        x_centered = tf.stack(x_centered_list)
+        covariance = K.dot(x_centered, K.transpose(x_centered)) / tf.cast(x_centered.get_shape()[0], tf.float32)
+        
+        return covariance
+            
+    # Constraint penalty
+    def uncorrelated_feature(self, x):
+        if(self.encoding_dim <= 1):
+            return 0.0
+        else:
+            output = K.sum(K.square(
+                self.covariance - tf.math.multiply(self.covariance, K.eye(self.encoding_dim))))
+            return output
+
+    def __call__(self, x):
+        self.covariance = self.get_covariance(x)
+        return self.weightage * self.uncorrelated_feature(x)
